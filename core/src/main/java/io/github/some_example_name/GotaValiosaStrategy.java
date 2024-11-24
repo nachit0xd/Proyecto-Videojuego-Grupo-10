1d193b39cc64f250ed3a6f8b57ae5ac4847846fb
R0bert20 <163351866+R0bert20@users.noreply.github.com>
2024-11-24 20:23:43 -0300

    Update GotaValiosaStrategy.java

--- core/src/main/java/io/github/some_example_name/GotaValiosaStrategy.java
+++ core/src/main/java/io/github/some_example_name/GotaValiosaStrategy.java
@@ -1,7 +1,8 @@
 package io.github.some_example_name;
 
 public class GotaValiosaStrategy implements GotaStrategy {
+
     public void aplicarEfecto(Tarro tarro) {
-        tarro.sumarPuntos(50); // Incrementa muchos puntos
+        tarro.sumarPuntos(50); // Incrementa 50 puntos
     }
 }
