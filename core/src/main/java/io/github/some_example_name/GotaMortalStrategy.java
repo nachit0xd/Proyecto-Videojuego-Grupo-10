09c3cf8e72f269815c6266ea49511588bdf098f2
R0bert20 <163351866+R0bert20@users.noreply.github.com>
2024-11-23 22:51:02 -0300

    Create GotaMortalStrategy.java
    Implementación del patrón strategy

---
+++ core/src/main/java/io/github/some_example_name/GotaMortalStrategy.java
@@ -0,0 +1,7 @@
+package io.github.some_example_name;
+
+public class GotaMortalStrategy implements GotaStrategy {
+    public void aplicarEfecto(Tarro tarro) {
+        tarro.terminarJuego(); // Termina el juego
+    }
+}
