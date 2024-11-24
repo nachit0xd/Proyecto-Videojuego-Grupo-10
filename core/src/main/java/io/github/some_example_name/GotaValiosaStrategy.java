697bcd7facff4eae6033daccfc49d8250387343f
R0bert20 <163351866+R0bert20@users.noreply.github.com>
2024-11-24 17:25:55 -0300

    Create GotaValiosaStrategy.java
    Se crea la clase  para implementar el patrón Strategy

---
+++ core/src/main/java/io/github/some_example_name/GotaValiosaStrategy.java
@@ -0,0 +1,7 @@
+package io.github.some_example_name;
+
+public class GotaValiosaStrategy implements GotaStrategy {
+    public void aplicarEfecto(Tarro tarro) {
+        tarro.sumarPuntos(50); // Incrementa muchos puntos
+    }
+}
