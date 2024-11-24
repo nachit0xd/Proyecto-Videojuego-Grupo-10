202c979c446fdee96a9ab0cbf22a728324d68e5f
R0bert20 <163351866+R0bert20@users.noreply.github.com>
2024-11-24 20:19:50 -0300

    Update GotaBuenaStrategy.java

--- core/src/main/java/io/github/some_example_name/GotaBuenaStrategy.java
+++ core/src/main/java/io/github/some_example_name/GotaBuenaStrategy.java
@@ -1,8 +1,15 @@
 package io.github.some_example_name;
+import com.badlogic.gdx.audio.Sound;
 
 public class GotaBuenaStrategy implements GotaStrategy {
-    @Override
+    private Sound dropSound;
+
+    public GotaBuenaStrategy(Sound dropSound) {
+        this.dropSound = dropSound;
+    }
+
     public void aplicarEfecto(Tarro tarro) {
-        tarro.sumarPuntos(10); // Incrementa los puntos
+        tarro.sumarPuntos(10); // Incrementa puntos
+        dropSound.play(); // Reproduce sonido
     }
 }
