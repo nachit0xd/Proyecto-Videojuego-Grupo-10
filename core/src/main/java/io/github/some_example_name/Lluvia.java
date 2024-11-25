aa881f9792c8a8384b3337d266f1e719389fee5f
R0bert20 <163351866+R0bert20@users.noreply.github.com>
2024-11-25 00:46:49 -0300

    Update Lluvia.java

--- core/src/main/java/io/github/some_example_name/Lluvia.java
+++ core/src/main/java/io/github/some_example_name/Lluvia.java
@@ -12,43 +12,45 @@ import com.badlogic.gdx.utils.TimeUtils;
 public class Lluvia {
     private Array<EntidadCaida> entidadesCaidas;
     private long lastDropTime;
+
     // Texturas de las gotas
     private Texture gotaBuena;
     private Texture gotaMala;
     private Texture gotaValiosa;
     private Texture gotaMortal;
+
     // Texturas de los poderes
     private Texture poderEscudo;
     private Texture poderVidaExtra;
+
     // Sonidos
     private Sound dropSound;
     private Sound dropValiosaSound;
     private Sound powerUpSound;
     private Music rainMusic;
+
     // Variables para alertas
     private long startTime;
     private boolean mostrarAlerta = false;
     private long tiempoInicioAlerta;
     private static final long DURACION_ALERTA = 5_000_000_000L;
 
-    // Constructor
     public Lluvia(Texture gotaBuena, Texture gotaMala, Texture gotaValiosa, Texture gotaMortal,
-                  Texture poderEscudo, Texture poderVidaExtra, Sound ss, Sound dropValiosaSound,
-                  Sound powerUpSound, Music mm) {
-        rainMusic = mm;
-        dropSound = ss;
-        this.dropValiosaSound = dropValiosaSound;
-        this.powerUpSound = powerUpSound;
+                  Texture poderEscudo, Texture poderVidaExtra, Sound dropSound, Sound dropValiosaSound,
+                  Sound powerUpSound, Music rainMusic) {
         this.gotaBuena = gotaBuena;
         this.gotaMala = gotaMala;
         this.gotaValiosa = gotaValiosa;
         this.gotaMortal = gotaMortal;
         this.poderEscudo = poderEscudo;
         this.poderVidaExtra = poderVidaExtra;
+        this.dropSound = dropSound;
+        this.dropValiosaSound = dropValiosaSound;
+        this.powerUpSound = powerUpSound;
+        this.rainMusic = rainMusic;
         this.startTime = TimeUtils.nanoTime();
     }
 
-    // Inicializa la lluvia
     public void crear() {
         entidadesCaidas = new Array<>();
         crearEntidadCaida();
@@ -56,95 +58,115 @@ public class Lluvia {
         rainMusic.play();
     }
 
-    // Genera una nueva entidad caída con estrategias
     private void crearEntidadCaida() {
-        float x = MathUtils.random(0, 800 - 64); // Posición aleatoria en el eje X
-        float y = 480; // Altura inicial
+        Directora directora = new Directora();
+        BuilderConcreto builder = new BuilderConcreto();
+        directora.setBuilder(builder);
 
-        GotaStrategy estrategia;
-        Texture textura;
-
-        int tipoEntidad = MathUtils.random(1, 1000);
+        Producto producto;
+        EntidadCaida entidad; // Aquí instanciamos la entidad final
+        int tipoEntidad = MathUtils.random(1, 100);
         long elapsedTime = TimeUtils.timeSinceNanos(startTime);
 
-        if (tipoEntidad <= 40) { // 4% de probabilidad
-            estrategia = new GotaValiosaStrategy();
-            textura = gotaValiosa;
-        } else if (tipoEntidad <= 640) { // 60% de probabilidad
-            estrategia = new GotaBuenaStrategy(dropSound);
-            textura = gotaBuena;
-        } else if (tipoEntidad <= 990) { // 35% de probabilidad
-            estrategia = new GotaMalaStrategy();
-            textura = gotaMala;
-        } else if (tipoEntidad <= 995) { // 0.5% de probabilidad
-            estrategia = new PoderEscudoStrategy();
-            textura = poderEscudo;
-        } else { // 0.5% de probabilidad
-            estrategia = new PoderVidaExtraStrategy();
-            textura = poderVidaExtra;
-        }
-
-        // Generar gotas mortales después de 30 segundos
-        if (elapsedTime > 30_000_000_000L) {
+        if (elapsedTime > 30_000_000_000L) { // Después de 30 segundos
             if (!mostrarAlerta) {
                 mostrarAlerta = true;
                 tiempoInicioAlerta = TimeUtils.nanoTime();
             }
-            if (tipoEntidad > 995) {
-                estrategia = new GotaMortalStrategy();
-                textura = gotaMortal;
+
+            if (tipoEntidad <= 5) { // 5% probabilidad para gotas mortales
+                producto = directora.construir("Mortal", gotaMortal, 500, 0);
+                entidad = new GotaMortal(producto.getTextura()); // Usamos textura configurada
+            } else if (tipoEntidad <= 15) { // 10% probabilidad para gotas valiosas
+                producto = directora.construir("Valiosa", gotaValiosa, 400, 50);
+                entidad = new GotaValiosa(producto.getTextura());
+            } else if (tipoEntidad <= 75) { // 60% probabilidad para gotas buenas
+                producto = directora.construir("Buena", gotaBuena, 300, 10);
+                entidad = new GotaBuena(producto.getTextura());
+            } else if (tipoEntidad <= 95) { // 20% probabilidad para gotas malas
+                producto = directora.construir("Mala", gotaMala, 200, -1);
+                entidad = new GotaMala(producto.getTextura());
+            } else if (tipoEntidad == 96) { // 1% probabilidad para poder escudo
+                producto = directora.construir("Escudo", poderEscudo, 150, 0);
+                entidad = new Poder(producto.getTextura(), new PoderEscudoStrategy());
+            } else { // 1% probabilidad para poder vida extra
+                producto = directora.construir("Vida Extra", poderVidaExtra, 150, 0);
+                entidad = new Poder(producto.getTextura(), new PoderVidaExtraStrategy());
+            }
+        } else { // Antes de 30 segundos (sin gotas mortales)
+            if (tipoEntidad <= 10) { // 10% probabilidad para gotas valiosas
+                producto = directora.construir("Valiosa", gotaValiosa, 400, 50);
+                entidad = new GotaValiosa(producto.getTextura());
+            } else if (tipoEntidad <= 70) { // 60% probabilidad para gotas buenas
+                producto = directora.construir("Buena", gotaBuena, 300, 10);
+                entidad = new GotaBuena(producto.getTextura());
+            } else if (tipoEntidad <= 95) { // 35% probabilidad para gotas malas
+                producto = directora.construir("Mala", gotaMala, 200, -1);
+                entidad = new GotaMala(producto.getTextura());
+            } else if (tipoEntidad == 96) { // 1% probabilidad para poder escudo
+                producto = directora.construir("Escudo", poderEscudo, 150, 0);
+                entidad = new Poder(producto.getTextura(), new PoderEscudoStrategy());
+            } else { // 1% probabilidad para poder vida extra
+                producto = directora.construir("Vida Extra", poderVidaExtra, 150, 0);
+                entidad = new Poder(producto.getTextura(), new PoderVidaExtraStrategy());
             }
         }
 
-        // Crear entidad genérica con estrategia asignada
-        GotaImpl entidad = new GotaImpl(x, y, estrategia, textura);
+        // Configurar posición y agregar la entidad
+        entidad.setPosition(MathUtils.random(0, 800 - 64), 480);
         entidadesCaidas.add(entidad);
-
         lastDropTime = TimeUtils.nanoTime();
     }
 
-    // Actualiza el movimiento y colisiones de las entidades caídas
+
+
+
     public boolean actualizarMovimiento(Tarro tarro) {
-        if (TimeUtils.nanoTime() - lastDropTime > 100000000L) {
-            crearEntidadCaida(); // Genera nuevas gotas cada 0.1 segundos
-        }
+        if (TimeUtils.nanoTime() - lastDropTime > 100000000) crearEntidadCaida();
 
         for (int i = 0; i < entidadesCaidas.size; i++) {
             EntidadCaida entidad = entidadesCaidas.get(i);
-            entidad.setPosition(entidad.getPosicion().x, entidad.getPosicion().y - 300 * Gdx.graphics.getDeltaTime());
+            entidad.getPosicion().y -= (entidad instanceof Poder ? 150 : 300) * Gdx.graphics.getDeltaTime();
 
-            // Elimina las gotas fuera de la pantalla
             if (entidad.getPosicion().y + 64 < 0) {
                 entidadesCaidas.removeIndex(i);
-                i--;
-                continue;
             }
 
-            // Maneja la colisión con el tarro
             if (entidad.getPosicion().overlaps(tarro.getArea())) {
-                entidad.efecto(tarro); // Aplica el efecto al tarro
-                entidadesCaidas.removeIndex(i);
-                i--;
-
-                // Verificar si el juego debe terminar
-                if (tarro.getVidas() <= 0) {
-                    return false;
+                if (entidad instanceof GotaMortal && tarro.isEscudoActivo()) {
+                    // Ignorar la gota mortal si el escudo está activo
+                    entidadesCaidas.removeIndex(i);
+                    continue;
                 }
+
+                entidad.efecto(tarro); // Aplicar el efecto normal
+                if (entidad instanceof Poder) {
+                    powerUpSound.play();
+                } else if (entidad instanceof GotaValiosa) {
+                    dropValiosaSound.play();
+                } else if (entidad instanceof GotaBuena) {
+                    dropSound.play();
+                }
+
+                entidadesCaidas.removeIndex(i);
+
+                // Terminar el juego si las vidas son cero
+                if (tarro.getVidas() <= 0) return false;
             }
         }
         return true;
     }
-
-    // Dibuja todas las gotas
+    
+    //Métodos de alertas
+    public boolean isMostrarAlerta() {
+        return mostrarAlerta && TimeUtils.timeSinceNanos(tiempoInicioAlerta) < DURACION_ALERTA;
+    }  
+    
+    //Actualiza el dibujo de las entidades caídas
     public void actualizarDibujoLluvia(SpriteBatch batch) {
         for (EntidadCaida entidad : entidadesCaidas) {
             entidad.dibujar(batch);
         }
-    }
-
-    // Métodos auxiliares
-    public boolean isMostrarAlerta() {
-        return mostrarAlerta && TimeUtils.timeSinceNanos(tiempoInicioAlerta) < DURACION_ALERTA;
     }
 
     public void destruir() {
