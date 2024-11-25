9b4c752cdf893fa3d243dc62ced1f8c8d1c7c7d5
R0bert20 <163351866+R0bert20@users.noreply.github.com>
2024-11-25 00:45:30 -0300

    Delete core/src/main/java/io/github/some_example_name/GotaMalaStrategy.java

--- core/src/main/java/io/github/some_example_name/GotaMalaStrategy.java
+++
@@ -1,9 +0,0 @@
-package io.github.some_example_name;
-
-public class GotaMalaStrategy implements GotaStrategy {
-    @Override
-    public void aplicarEfecto(Tarro tarro) {
-        tarro.dañar(); // Resta una vida
-    }
-}
-
