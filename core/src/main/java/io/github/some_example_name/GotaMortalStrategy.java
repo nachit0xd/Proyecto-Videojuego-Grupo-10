95bd7ec71a4391ed6033830769881fa775fac06d
R0bert20 <163351866+R0bert20@users.noreply.github.com>
2024-11-25 00:45:50 -0300

    Delete core/src/main/java/io/github/some_example_name/GotaMortalStrategy.java

--- core/src/main/java/io/github/some_example_name/GotaMortalStrategy.java
+++
@@ -1,7 +0,0 @@
-package io.github.some_example_name;
-
-public class GotaMortalStrategy implements GotaStrategy {
-    public void aplicarEfecto(Tarro tarro) {
-        tarro.setVidas(0); // Reduce las vidas a 0 (game over)
-    }
-}
