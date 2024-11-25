49173ca050bd924ef8d0e57d0b4f92e1bca237a1
R0bert20 <163351866+R0bert20@users.noreply.github.com>
2024-11-25 00:46:11 -0300

    Delete core/src/main/java/io/github/some_example_name/GotaValiosaStrategy.java

--- core/src/main/java/io/github/some_example_name/GotaValiosaStrategy.java
+++
@@ -1,8 +0,0 @@
-package io.github.some_example_name;
-
-public class GotaValiosaStrategy implements GotaStrategy {
-
-    public void aplicarEfecto(Tarro tarro) {
-        tarro.sumarPuntos(50); // Incrementa 50 puntos
-    }
-}
