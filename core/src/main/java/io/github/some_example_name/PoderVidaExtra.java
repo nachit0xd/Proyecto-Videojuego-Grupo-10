7a33c6311a75e27b13dd87838eda97aa4d8bd8e4
R0bert20 <163351866+R0bert20@users.noreply.github.com>
2024-11-25 01:25:07 -0300

    Update PoderVidaExtra.java

--- core/src/main/java/io/github/some_example_name/PoderVidaExtra.java
+++ core/src/main/java/io/github/some_example_name/PoderVidaExtra.java
@@ -1,1 +1,8 @@
-s
+package io.github.some_example_name;
+
+public class PoderVidaExtra implements Poder {
+    @Override
+    public void efecto(Tarro tarro) {
+        tarro.incrementarVida(1); // Incrementa una vida al tarro
+    }
+}
