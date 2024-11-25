6291f757a924af789c7c418e47404a00dd701ee1
R0bert20 <163351866+R0bert20@users.noreply.github.com>
2024-11-25 00:51:02 -0300

    Update PoderVidaExtraStrategy.java

--- core/src/main/java/io/github/some_example_name/PoderVidaExtraStrategy.java
+++ core/src/main/java/io/github/some_example_name/PoderVidaExtraStrategy.java
@@ -1,9 +1,8 @@
 package io.github.some_example_name;
 
-public class PoderVidaExtraStrategy implements GotaStrategy {
-    private static final int VIDAS_EXTRA = 1; // Cantidad de vidas adicionales
-
+public class PoderVidaExtraStrategy implements PoderStrategy {
+    @Override
     public void aplicarEfecto(Tarro tarro) {
-        tarro.incrementarVida(VIDAS_EXTRA); // Incrementa las vidas del tarro
+        tarro.incrementarVida(1); // Incrementa una vida al tarro
     }
 }
